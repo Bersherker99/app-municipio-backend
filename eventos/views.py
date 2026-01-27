9e4d76f42b270ec4e299a6dc0e4eddf8c70f5c37
Bersherker99 <carlosquintigb.2017@gmail.com>
2026-01-27 09:55:26 -0500

    Agregando vista detalle_evento faltante

--- eventos/views.py
+++ eventos/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
 from .models import Evento
 from .serializers import EventoSerializer
+# 👇 AGREGA ESTAS 2 LÍNEAS NUEVAS AL PRINCIPIO 👇
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('fecha_inicio')
     serializer_class = EventoSerializer
+
+# 👇 AGREGA ESTO AL FINAL DEL ARCHIVO (FUERA DE LA CLASE) 👇
+def detalle_evento(request, evento_id):
+    evento = get_object_or_404(Evento, pk=evento_id)
+    return HttpResponse(f"<h1>Detalle del Evento {evento_id}</h1>")
