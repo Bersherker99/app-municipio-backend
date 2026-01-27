530c5e22f6897bd59ea42af144caff01b0ff65f6
Bersherker99 <carlosquintigb.2017@gmail.com>
2026-01-27 11:18:05 -0500

    Correccion final CSRF

--- backend_municipio/settings.py
+++ backend_municipio/settings.py
@@ -139,3 +139,4 @@ if 'DATABASE_URL' in os.environ:
 # 2. Archivos Estáticos (Imágenes y CSS)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CSRF_TRUSTED_ORIGINS = ['https://web-production-34d7e.up.railway.app']
